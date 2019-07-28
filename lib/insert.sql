49004073a9052b9cfa53858dca4a5474e3ca2672
Ellyn <eka249@nyu.edu>
2019-07-28 01:43:13 -0700

    commit

---
+++ lib/insert.sql
@@ -0,0 +1,194 @@
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+    VALUES
+    (1, "Project 1", "Elephants", 20000, "Aug 1", "Aug 31");
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+    VALUES
+    (2, "Project 2", "Children", 30000, "Sept 1", "Sept 31");
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+    VALUES
+    (3, "Project 3", "Monkeys", 10000, "April 1", "April 31");
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+    VALUES
+    (4, "Project 4", "Lions", 40000, "July 1", "July 31");
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+    VALUES
+    (5, "Project 5", "Low income", 50000, "Dec 1", "Dec 31");
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+    VALUES
+    (6, "Project 6", "Inner city", 60000, "Jan 1", "Jan 31");
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+    VALUES
+    (7, "Project 7", "Women", 70000, "Feb 1", "Feb 31");
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+    VALUES
+    (8, "Project 8", "Polar bears", 80000, "March 1", "Mar 31");
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+    VALUES
+    (9, "Project 9", "Diseases", 90000, "Oct 1", "Oct 31");
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+    VALUES
+    (10, "Project 10", "Personal", 20, "May 1", "May 31");
+
+
+--users------------------------------------------
+INSERT INTO users VALUES
+    (1, "Name 1", 20);
+
+INSERT INTO users VALUES
+    (2, "Name 2", 20);
+
+INSERT INTO users VALUES
+    (3, "Name 3", 20);
+
+INSERT INTO users VALUES
+    (4, "Name 4", 20);
+
+INSERT INTO users VALUES
+    (5, "Name 5", 20);
+
+INSERT INTO users VALUES
+    (6, "Name 6", 20);
+
+INSERT INTO users VALUES
+    (7, "Name 7", 20);
+
+INSERT INTO users VALUES
+    (8, "Name 8", 20);
+
+INSERT INTO users VALUES
+    (9, "Name 9", 20);
+
+INSERT INTO users VALUES
+    (10, "Name 10", 20);
+
+INSERT INTO users VALUES
+    (11, "Name 11", 20);
+
+INSERT INTO users VALUES
+    (12, "Name 12", 20);
+
+INSERT INTO users VALUES
+    (13, "Name 13", 20);
+
+INSERT INTO users VALUES
+    (14, "Name 14", 20);
+
+INSERT INTO users VALUES
+    (15, "Name 15", 20);
+
+INSERT INTO users VALUES
+    (16, "Name 16", 20);
+
+INSERT INTO users VALUES
+    (17, "Name 17", 20);
+
+INSERT INTO users VALUES
+    (18, "Name 18", 20);
+
+INSERT INTO users VALUES
+    (19, "Name 19", 20);
+
+INSERT INTO users VALUES
+    (20, "Name 20", 20);
+
+
+--pleges--------------------------------------
+
+INSERT INTO pledges VALUES
+    (1, 20, 1, 3);
+
+INSERT INTO pledges VALUES
+    (2, 20, 1, 3);
+
+INSERT INTO pledges VALUES
+    (3, 20, 1, 3);
+
+INSERT INTO pledges VALUES
+    (4, 20, 1, 3);
+
+INSERT INTO pledges VALUES
+    (5, 20, 1, 3);
+
+INSERT INTO pledges VALUES
+    (6, 20, 2,8);
+
+INSERT INTO pledges VALUES
+    (7, 20, 2,8);
+
+INSERT INTO pledges VALUES
+    (8, 20, 2,8);
+
+INSERT INTO pledges VALUES
+    (9, 20, 2,8);
+
+INSERT INTO pledges VALUES
+    (10, 20, 2,8);
+
+INSERT INTO pledges VALUES
+    (11, 20, 2,8);
+
+INSERT INTO pledges VALUES
+    (12, 20, 10, 1);
+
+INSERT INTO pledges VALUES
+    (13, 20, 10, 1);
+
+INSERT INTO pledges VALUES
+    (14, 20, 10, 1);
+
+INSERT INTO pledges VALUES
+    (15, 20, 10, 1);
+
+INSERT INTO pledges VALUES
+    (16, 20, 11, 1);
+
+INSERT INTO pledges VALUES
+    (17, 20, 11, 1);
+
+INSERT INTO pledges VALUES
+    (18, 20, 11, 1);
+
+INSERT INTO pledges VALUES
+    (19, 20, 8, 6);
+
+INSERT INTO pledges VALUES
+    (20, 20, 8, 6);
+
+INSERT INTO pledges VALUES
+    (21, 20, 8, 6);
+
+INSERT INTO pledges VALUES
+    (22, 20, 8, 6);
+
+INSERT INTO pledges VALUES
+    (23, 20, 8, 6);
+
+INSERT INTO pledges VALUES
+    (24, 20, 8, 6);
+
+INSERT INTO pledges VALUES
+    (25, 20, 8, 6);
+
+INSERT INTO pledges VALUES
+    (26, 20, 8, 6);
+
+INSERT INTO pledges VALUES
+    (27, 20, 8, 6);
+
+INSERT INTO pledges VALUES
+    (28, 20, 8, 6);
+
+INSERT INTO pledges VALUES
+    (29, 20, 8, 6);
+
+INSERT INTO pledges VALUES
+    (30, 20, 8, 6);
